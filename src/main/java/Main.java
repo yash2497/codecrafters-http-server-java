f4ab74c78ca2b8b47979c080d97cdab06e47f8bc
Shivalik Singh <singhshivalik082@gmail.com>
2024-07-24 23:06:33 +0530

    pass 5th stage,draft

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -77,7 +77,7 @@ public class Main {
         // Extract path
         String path = parts[1];
         System.out.println("path: " + path);
-        if(path.startsWith("/user-agent")) {
+        if(path.startsWith("/echo")) {
             path = path.startsWith("/") ? path.substring(1) : path;
             String[] params = path.split("/");
             return params[1].startsWith("/") ? path.substring(1) : params[1];
